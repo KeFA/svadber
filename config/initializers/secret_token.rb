da5115c0301de45236fbae659be2286f37f186c1
Aliaksei Sinkevich <tokefa@gmail.com>
2013-01-07 14:13:10 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Svadber::Application.config.secret_token = 'a21db2470f4a452b55e399d5f5d94a5c0893b8c0894de100856aaae66e92c1601033094a5a8fcaf0090a8ac64c6548d0b0f40f295c3f5b2b478e86895b014fac'
