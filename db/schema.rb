e04a9efdd95834258139ad5cf6e72fdd4a53d0b5
Aliaksei Sinkevich <tokefa@gmail.com>
2013-08-25 00:20:34 +0300

    Add bin folder from Rails 4

--- db/schema.rb
+++ db/schema.rb
@@ -9,64 +9,64 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130520175120) do
+ActiveRecord::Schema.define(version: 20130520175120) do
 
-  create_table "check_items", :force => true do |t|
+  create_table "check_items", force: true do |t|
     t.string   "description"
     t.boolean  "done"
     t.integer  "wedding_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table "expenditures", :force => true do |t|
+  create_table "expenditures", force: true do |t|
     t.integer  "wedding_id"
-    t.integer  "cost",        :default => 0
+    t.integer  "cost",        default: 0
     t.string   "description"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
-    t.integer  "paid",        :default => 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "paid",        default: 0
   end
 
-  create_table "guests", :force => true do |t|
+  create_table "guests", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "middle_name"
     t.string   "city"
     t.string   "status"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "wedding_id"
   end
 
-  add_index "guests", ["wedding_id"], :name => "index_guests_on_wedding_id"
+  add_index "guests", ["wedding_id"], name: "index_guests_on_wedding_id"
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
-  create_table "weddings", :force => true do |t|
+  create_table "weddings", force: true do |t|
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "weddings", ["user_id"], :name => "index_weddings_on_user_id"
+  add_index "weddings", ["user_id"], name: "index_weddings_on_user_id"
 
 end
