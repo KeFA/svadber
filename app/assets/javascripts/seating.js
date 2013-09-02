27f3258682a76fa0ab99c1b33547a6b8cb44a0a9
Maryia Sinkevich <myr-kat@bk.ru>
2013-09-03 00:45:04 +0300

    Guest drag and drop started, editing support for tables and grid were added

--- app/assets/javascripts/seating.js
+++ app/assets/javascripts/seating.js
@@ -1,13 +1,20 @@
-function Table(tableNumber, guests) {
+function Table(tableNumber, guests, HTMLTable, tableLength) {
     this.chairs = [];
     this.tableNumber = tableNumber;
+    this.HTMLTable = HTMLTable;
+    this.tableWidth = HTMLTable.width();
     this.initTable = function () {
         for (var i = 0; i < guests.length; i++) {
             var currentGuest = guests[i];
             var currentChair = new Chair(i, currentGuest);
             this.chairs.push(currentChair);
         }
-        console.log(this.chairs);
+    };
+    this.initEmptyTable = function () {
+        for (var i = 0; i < tableLength; i++) {
+            var currentChair = new Chair(i, {});
+            this.chairs.push(currentChair);
+        }
     };
 }
 
@@ -19,10 +26,14 @@ function Chair(number, guest) {
 var Seating = function(){
     var settings = {
         drawPanel:{
-            name: $(".seating-panel"),
-            rectangleTable: $("#seating-panel-rectangle-table")
+            name: $(".seating-panel")
+
         },
-        drawArea: $(".seating-area"),
+        drawArea: {
+            name: $(".seating-area"),
+            gridWidth: 50,
+            tableDraggableIcon: "seating-table-"
+        },
         tables: {
             tableStartNumber: 1,
             tableStartName: "seating-table-",
@@ -32,32 +43,70 @@ var Seating = function(){
     };
 
     var init = function() {
-        enablePanelRectangleTable();
+        enableGuestsMoving();
+        createNewIconTable();
     };
 
-    var enablePanelRectangleTable = function() {
-        var table = settings.drawPanel.rectangleTable;
-        table.draggable({
-            drag: function() {
-                table.draggable('destroy');
-                var table1guests = ["Синкевич Мария Сергеевна", "Синкевич Алексей Викторович", "Мисан Андрей Викторович"],
-                    table2guests = ["Синкевич Ольга Борисовна", "Синкевич Виктор Семенович", "Рудько Елена Владимировна", "Рудько Сергей Петрович"];
-
-                createNewTable(settings.tables.tableStartNumber, table1guests);
-                enablePanelRectangleTable();
-            }
+    var enableGuestsMoving = function() {
+        $(".seating-quest-icon").draggable({
+            grid: [settings.drawArea.gridWidth, settings.drawArea.gridWidth],
+            revert: 'invalid'
         });
 
     };
 
-    var createNewTable = function(index, tableGuests) {
-        var table = new Table(index, tableGuests),
-            tableID = settings.tables.tableStartName + index;
-        table.initTable();
+    var enablePanelRectangleTable = function() {
+//        var table = "#" + settings.drawArea.tableDraggableIcon + settings.tables.tableStartNumber;
+//            console.log(table);
+//        $(table).draggable({
+//            create: function(){
+//                console.log("create");
+//                alert("ee");
+//                var table1guests = ["Синкевич Мария Сергеевна", "Синкевич Алексей Викторович", "Мисан Андрей Викторович"],
+//                    table2guests = ["Синкевич Ольга Борисовна", "Синкевич Виктор Семенович", "Рудько Елена Владимировна", "Рудько Сергей Петрович"];
+//                createNewTable(table1guests);
+//
+//            },
+//            start: function(event) {
+//                $(this).addClass("active");
+//            }
+//        });
+
+    };
+
+    var createNewIconTable = function() {
+        var tableID = settings.tables.tableStartName + settings.tables.tableStartNumber,
+            newHTMLTable = $(settings.tables.tableHTML).attr("id", tableID).draggable({
+            grid: [settings.drawArea.gridWidth, settings.drawArea.gridWidth],
+            create: function(){
+                settings.drawArea.name.append(this);
+            },
+            start: function(event) {
+                if (!$(this).hasClass("active")) {
+
+                    var table1guests = ["Синкевич Мария Сергеевна", "Синкевич Алексей Викторович", "Мисан Андрей Викторович"],
+                        table2guests = ["Синкевич Ольга Борисовна", "Синкевич Виктор Семенович", "Рудько Елена Владимировна", "Рудько Сергей Петрович"];
+                    $(this).addClass("active").resizable({
+                        grid: [settings.drawArea.gridWidth, settings.drawArea.gridWidth]
+                    });
+                    console.log($(this).width());
+                    createNewTable(table1guests, $(this));
+                    createNewIconTable();
+                }
+            }
+        });
+    };
+
+    var createNewTable = function(tableGuests, HTMLTable) {
+        var table = new Table(settings.tables.tableStartNumber++, tableGuests, HTMLTable, 8);
+        table.initEmptyTable();
+        drawEmptyChairs(table);
         settings.tables.tableList.push(table);
-        settings.tables.tableStartNumber++;
-        var newHTMLTable = $(settings.tables.tableHTML).attr("id", tableID).draggable();
-        settings.drawArea.append(newHTMLTable);
+        console.log(table);
+    };
+
+    var drawEmptyChairs = function(table) {
+
     };
 
     return {
