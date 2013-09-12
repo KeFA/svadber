3a2bca9e5dad7c0ce428c8f19c60c2c90ecb242e
Aliaksei Sinkevich <tokefa@gmail.com>
2013-09-13 00:49:17 +0300

    Add karma for testing js

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Todo.create(done: false, description: 'todo 1', wedding_id: 1)
-Todo.create(done: true, description: 'todo 2', wedding_id: 1)
-Todo.create(done: false, description: 'todo 3', wedding_id: 1)
+puts 'Creating test user...'
+User.create(id: 234234, email: 'test@admin.by', password: 'warmoon', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: '2013-09-12 21:01:09', last_sign_in_at: '2013-09-12 21:01:09', current_sign_in_ip: '127.0.0.1', last_sign_in_ip: '127.0.0.1', created_at: '2013-09-12 21:01:09', updated_at: '2013-09-12 21:01:09')
