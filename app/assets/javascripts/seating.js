00bc652c6889a3df681979e05a62d832bcdee398
Maryia Rudzko <myr-kat@bk.ru>
2013-09-15 14:14:18 +0300

    Global refactoring (1st part)

--- app/assets/javascripts/seating.js
+++ app/assets/javascripts/seating.js
@@ -19,7 +19,8 @@ var Seating = function () {
                 startNumber: 1,
                 htmlClass: "seating-table-number"
             },
-            htmlClass: "seating-panel-table",
+            htmlClass: "seating-table",
+            activeClass: "active",
             tableList: []
         },
         chairs: {
@@ -27,145 +28,152 @@ var Seating = function () {
                 startNumber: 1,
                 htmlClass: "seating-table-number"
             },
-            htmlClass: "seating-panel-chair"
+            htmlClass: "seating-chair"
+        },
+        guests: {
+            icon: {
+                htmlClass: "seating-quest-icon",
+                $htmlClass: ""
+            }
         }
     };
 
     var init = function () {
         enableGuestsMoving();
-        createNewIconTable();
+        createNewTable();
     };
 
     var enableGuestsMoving = function () {
-        $(".seating-quest-icon").draggable({
+        settings.guests.icon.$htmlClass = $("." + settings.guests.icon.htmlClass);
+        settings.guests.icon.$htmlClass.draggable({
             grid: [settings.drawArea.gridWidth, settings.drawArea.gridWidth],
             revert: "invalid"
         });
 
     };
 
-    var createNewIconTable = function () {
-        var tableID = settings.tables.htmlClass + "-" + settings.tables.number.startNumber;
-        $(settings.htmlTags.emptyDiv).
-            addClass(settings.tables.htmlClass).
-            attr("id", tableID).
-            draggable({
-                grid: [settings.drawArea.gridWidth, settings.drawArea.gridWidth],
-                containment: settings.drawArea.name,
-                create: function () {
-                    settings.drawArea.name.append(this);
-                },
-                start: function (event) {
-                    var $this = $(this);
-                    if (!$this.hasClass("active")) {
-                        $this.addClass("active");
-                        var currentTable = createNewTable({}, $(this));
-                        createNewIconTable();
-                        $this.resizable({
-                            grid: [settings.drawArea.gridWidth, settings.drawArea.gridWidth],
-                            resize: function () {
-                                currentTable.resizeTable();
-                            }
-                        });
-                    }
-                },
-                stop: function () {
-
-                }
-            });
-    };
-
-    var createNewTable = function (tableGuests, tableHTML) {
-        var table = new Table(settings.tables.number.startNumber++, tableGuests, tableHTML);
-        table.init();
+    var createNewTable = function () {
+        var table = new Table(settings.tables.number.startNumber++).init();
         return table;
     };
 
-    function Table(tableNumber, guests, tableHTML) {
+    function Table(tableNumber) {
         this.chairs = [];
         this.tableNumber = tableNumber;
-        this.tableHTML = tableHTML;
-        this.tableWidth = tableHTML.width();
+        this.id = settings.tables.htmlClass + "-" + this.tableNumber;
+        this.$id = $("#" + this.id);
         this.init = function () {
+            this.createHTMLTable();
+            this.makeDraggable();
+        };
+        this.activateTable = function () {
             this.drawTableNumber();
             this.addChairs();
             settings.tables.tableList.push(this);
-            console.log(this.getTableChairs());
         };
-        this.initTable = function () {
-            for (var i = 0; i < guests.length; i++) {
-                var currentGuest = guests[i];
-                var currentChair = new Chair(i, currentGuest);
-                this.chairs.push(currentChair);
-            }
-        };
-        this.resizeTable = function () {
-            console.log("dfgdfg");
+        this.createHTMLTable = function () {
+            $(settings.htmlTags.emptyDiv).addClass(settings.tables.htmlClass).attr("id", this.id).appendTo(settings.drawArea.name);
+            this.$id = $("#" + this.id);
         };
         this.drawTableNumber = function () {
             var tableNumberClass = settings.tables.htmlClass + settings.number.htmlClassPart,
                 tableNumberDivId = settings.tables.htmlClass + "-" + this.tableNumber + settings.number.htmlClassPart;
-            $(settings.htmlTags.emptyDiv).appendTo(this.tableHTML).addClass(tableNumberClass).attr("id", tableNumberDivId).text(this.tableNumber);
-
+            $(settings.htmlTags.emptyDiv).appendTo(this.$id).addClass(tableNumberClass).attr("id", tableNumberDivId).text(this.tableNumber);
+        };
+        this.makeDraggable = function () {
+            var self = this;
+            self.$id.draggable({
+                grid: [settings.drawArea.gridWidth, settings.drawArea.gridWidth],
+                containment: settings.drawArea.name,
+                start: function (event) {
+                    var $this = $(this);
+                    if (!$this.hasClass(settings.tables.activeClass)) {
+                        $this.addClass(settings.tables.activeClass);
+                        self.activateTable();
+                        createNewTable();
+                        $this.resizable({
+                            grid: [settings.drawArea.gridWidth, settings.drawArea.gridWidth],
+                            resize: function () {
+                                $this.resizeTable();
+                            }
+                        });
+                    }
+                }
+            });
         };
         this.getTableWidth = function () {
-            return this.tableHTML.width();
+            return this.$id.width();
         };
         this.getTableHeight = function () {
-            return this.tableHTML.height();
+            return this.$id.height();
         };
         this.getTableChairs = function () {
             return (this.getTableWidth() / settings.drawArea.gridWidth) * 2 + (this.getTableHeight() / settings.drawArea.gridWidth) * 2;
-        }
+        };
+        this.resizeTable = function () {
+            console.log("dfgdfg");
+        };
         this.addChairs = function () {
             var chairIndex = 1,
                 self = this;
-            for (var i = 0; i < this.getTableWidth() / settings.drawArea.gridWidth; i++) {
-                var newChair = new Chair(chairIndex++, this.tableNumber, "");
-                this.tableHTML.append(newChair.chairHTML);
-                $("#" + newChair.id).css("left", settings.drawArea.gridWidth * i);
-                newChair.init();
-            }
-            for (var i = 0; i < this.getTableWidth() / settings.drawArea.gridWidth; i++) {
-                var newChair = new Chair(chairIndex++, this.tableNumber, "");
-                this.tableHTML.append(newChair.chairHTML);
-                $("#" + newChair.id).css("left", settings.drawArea.gridWidth * i).css("top", self.getTableHeight());
-                newChair.init();
-            }
-            for (var i = 0; i < this.getTableHeight() / settings.drawArea.gridWidth; i++) {
-                var newChair = new Chair(chairIndex++, this.tableNumber, "");
-                this.tableHTML.append(newChair.chairHTML);
-                $("#" + newChair.id).css("left", -settings.drawArea.gridWidth).css("top", settings.drawArea.gridWidth * i);
-                newChair.init();
-            }
-            for (var i = 0; i < this.getTableHeight() / settings.drawArea.gridWidth; i++) {
-                var newChair = new Chair(chairIndex++, this.tableNumber, "");
-                this.tableHTML.append(newChair.chairHTML);
-                $("#" + newChair.id).css("left", self.getTableWidth()).css("top", settings.drawArea.gridWidth * i);
-                newChair.init();
-            }
+            new Chair(chairIndex++, this, "").init();
+            console.log(this.getTableWidth());
+            console.log(this.getTableHeight());
+
+//            var chairIndex = 1,
+//                self = this;
+//            for (var i = 0; i < this.getTableWidth() / settings.drawArea.gridWidth; i++) {
+//                var newChair = new Chair(chairIndex++, this, "");
+//                this.$id.append(newChair.chairHTML);
+//                $("#" + newChair.id).css("left", settings.drawArea.gridWidth * i);
+//                newChair.init();
+//            }
+//            for (var i = 0; i < this.getTableWidth() / settings.drawArea.gridWidth; i++) {
+//                var newChair = new Chair(chairIndex++, this, "");
+//                this.$id.append(newChair.chairHTML);
+//                $("#" + newChair.id).css("left", settings.drawArea.gridWidth * i).css("top", self.getTableHeight());
+//                newChair.init();
+//            }
+//            for (var i = 0; i < this.getTableHeight() / settings.drawArea.gridWidth; i++) {
+//                var newChair = new Chair(chairIndex++, this., "");
+//                this.$id.append(newChair.chairHTML);
+//                $("#" + newChair.id).css("left", -settings.drawArea.gridWidth).css("top", settings.drawArea.gridWidth * i);
+//                newChair.init();
+//            }
+//            for (var i = 0; i < this.getTableHeight() / settings.drawArea.gridWidth; i++) {
+//                var newChair = new Chair(chairIndex++, this, "");
+//                this.$id.append(newChair.chairHTML);
+//                $("#" + newChair.id).css("left", self.getTableWidth()).css("top", settings.drawArea.gridWidth * i);
+//                newChair.init();
+//            }
         };
     }
 
-    function Chair(chairNumber, tableNumber, guest) {
+    function Chair(chairNumber, table, guest) {
         this.chairNumber = chairNumber;
-        this.tableNumber = tableNumber;
+        this.tableNumber = table.tableNumber;
         this.guest = guest;
         this.id = settings.tables.htmlClass + "-" + this.tableNumber + "-" + settings.chairs.htmlClass + "-" + this.chairNumber;
-        this.chairHTML = $(settings.htmlTags.emptyDiv).addClass(settings.chairs.htmlClass).attr("id", this.id);
+        this.$id = $("#" + this.id);
         this.init = function () {
+            this.createHTMLChair();
             this.drawChairNumber();
             this.makeDroppable();
         };
+        this.createHTMLChair = function () {
+            $(settings.htmlTags.emptyDiv).addClass(settings.chairs.htmlClass).attr("id", this.id).appendTo(table.$id);
+            this.$id = $("#" + this.id);
+        };
         this.drawChairNumber = function () {
-            //    chairNumberDivId = settings.tables.htmlClass + "-" + this.tableNumber + settings.tables.htmlClass + "-" + this.tableNumber + settings.number.htmlClassPart;
-            //$(settings.htmlTags.emptyDiv).appendTo(this.tableHTML).addClass(tableNumberClass).attr("id", tableNumberDivId).text(this.tableNumber);
+            var chairNumberClass = settings.chairs.htmlClass + settings.number.htmlClassPart,
+                chairNumberDivId = this.id + settings.number.htmlClassPart;
+            $(settings.htmlTags.emptyDiv).appendTo(this.$id).addClass(chairNumberClass).attr("id", chairNumberDivId).text(this.chairNumber);
         };
         this.makeDroppable = function () {
-            $("#" + this.id).droppable({
-                accept: ".seating-quest-icon",
+            this.$id.droppable({
+                accept: "." + settings.guests.icon.htmlClass,
                 drop: function (event, ui) {
-                    var seat = $(this),
+                    var seat = this,
                         guestIcon = ui.draggable,
                         guestName = guestIcon.data("guest");
                     guestIcon.appendTo(seat).css("left", settings.drawArea.gridWidth / 2).css("top", settings.drawArea.gridWidth / 2).text(guestName);
